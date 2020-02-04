92cf176550c8a00a5f4a975c32bd31c48f630c5c
Muflih Nadzarudin Majid <BSI90400@bsi.co.id>
2020-02-04 09:54:57 +0700

    update

--- lib/data/infrastructures/db_service_interface.dart
+++
@@ -1,6 +0,0 @@
-abstract class DBServiceInterface {
-  Future<int> insert(String sql);
-  Future<int> update(String sql);
-  Future<int> delete(String sql);
-  Future<List<Map>> select(String sql);
-}
